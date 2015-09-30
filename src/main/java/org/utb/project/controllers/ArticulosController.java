700b4d855759b9d65e27f2bdc4885a501f4866a0
sala403e15 <sala403e15@SALA403-E17.unitecnologica.edu.co>
2015-09-30 18:55:35 -0500

    Commit inicial

---
+++ src/main/java/org/utb/project/controllers/ArticulosController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.utb.project.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.utb.project.dao.ArticuloDao;
+import org.utb.project.entities.Articulo;
+
+/**
+ *
+ * @author Juansa
+ */
+@Controller
+public class ArticulosController {
+    @Autowired
+    ArticuloDao articuloDao;
+    
+    @RequestMapping(value = "/articulos/listado", method = RequestMethod.GET)
+    public String listado(){
+        return "articulos/listado";
+    }
+    
+    @RequestMapping(path = "/api/articulos", method = RequestMethod.POST)
+    @ResponseBody
+    public void guardar(@ModelAttribute Articulo articulo) {
+        articuloDao.agregar(articulo);
+    }
+    
+    @RequestMapping(path = "/api/articulos/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public void editar(@PathVariable Long id, @ModelAttribute Articulo articulo){
+        articulo.setId(id);
+        articuloDao.editar(articulo);
+    }
+    
+    @RequestMapping(path = "/api/articulos/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void borrar(@PathVariable Long id){
+        articuloDao.borrar(id);
+    }
+    
+    @RequestMapping(path = "/api/articulos", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<Articulo>listar(){
+        return articuloDao.listar();
+    }
+    
+    @RequestMapping(path = "/api/articulos/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Articulo obtener(@PathVariable("id") Long id){
+        return articuloDao.obtener(id);
+    }
+}
